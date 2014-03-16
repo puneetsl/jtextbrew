77c181c99e35e4de49450253d9121276a523b2eb
puneetsl <everlasting.puneet@gmail.com>
2014-03-16 15:11:51 -0400

    Adding few more test cases and edit in the Readme

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 textbrew
 ========
 
-Java Library for fuzzy string matching using TextBrew
-
+Java Library for fuzzy string matching using TextBrew.<br>
+Library is usable for general use of fuzzy string matching, and can even work for abbreviations and shorthands such as:<br>
+`clmbs blvd` and  `columbus boulevard`
 
 ## TODO ##
 * Validate the traceback path
+* Sometimes the traceback path is different from the test data
 
 ## References ##
 [1] http://www.ling.ohio-state.edu/~cbrew/795M/string-distance.html<br>
--- src/test/Main.java
+++ src/test/Main.java
@@ -3,8 +3,11 @@ package test;
 import info.puneetsingh.fsm.TextBrew;
 public class Main {
 	public static void main(String[] args) {
-		System.out.println(TextBrew.compare("BRYN MAWR PIZZA II", "TASTY PLACE"));
+		System.out.println(TextBrew.compare("BRYN MAWR PIZZA II", "BR MA PI II"));
 		System.out.println(TextBrew.compare("clmbs blvd", "columbus boulevard"));
 		System.out.println(TextBrew.compare("hosp", "hospital"));
+		System.out.println(TextBrew.compare("", "hospital"));
+		System.out.println(TextBrew.compare("ko", "no"));
+		System.out.println(TextBrew.compare("hojpithol", "hospital"));
 	}
 }
